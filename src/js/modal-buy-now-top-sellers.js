c2c665b418230b99dc1e24fff5ae021fe7186dc4
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-06-19 19:17:51 +0300

    removed closing button and click event fot it in js-code fot top-sellers buy btn

--- src/js/modal-buy-now-top-sellers.js
+++ src/js/modal-buy-now-top-sellers.js
@@ -1,12 +1,10 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-buy-now-top-open]"),
-    closeModalBtn: document.querySelector("[data-modal-buy-now-top-close]"),
-    modal: document.querySelector("[data-modal-buy-now-top]"),
+    modal: document.querySelector("[data-modal-buy-now]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
